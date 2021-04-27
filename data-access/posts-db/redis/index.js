f560e6cbf06457c1abafeb3594d537fe3a522709
junhonglee <junhong199@gmail.com>
2021-04-27 19:24:55 +0900

    fix mis-used syntax -> return await

--- data-access/posts-db/redis/index.js
+++ data-access/posts-db/redis/index.js
@@ -2,15 +2,14 @@
 const redisClient = require('../../../db/redis/models/post');
 const makePost = require('../../../models/post/index') // model
 
-let findPost = async (postId) => {
-    return await redisClient.hgetallAsync(`id:${postId}`);
+let findPost = (postId) => {
+    return redisClient.hgetallAsync(`id:${postId}`);
 }
 
-let addPost = async (postInfo) => {
+let addPost = (postInfo) => {
     const postMaker = makePost();
     const post = postMaker(postInfo);
-    await redisClient.hmsetAsync(post);
-    return await redisClient.hgetallAsync(`id:${post['id']}`);
+    return redisClient.hmsetAsync(post);
 }
 
 module.exports = {
